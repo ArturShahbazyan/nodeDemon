0e94160a1ca01954591d7105e784eb395ccfcd3c
ArturSh0101 <artursh0101@gmail.com>
2022-01-09 18:48:02 +0400

    The function, which gets a copy of the object where the keys have become the values and the values become the keys, is improved with recursion

--- v1.1_task.js
+++ v1.1_task.js
@@ -1,8 +1,42 @@
+// const obj = {
+//   name: "Jhon",
+//   country: {
+//     name: "Armenia",
+//     code: 374,
+//   },
+// };
+
+// function changeObjectKeysWithValues(obj) {
+//   let nestedChangedObj = {};
+
+//   let newObj = {};
+
+//   for (key in obj) {
+//     nestedChangedObj = Object.entries(obj[key]).reduce(
+//       (accumulator, [key, value]) => ((accumulator[value] = key), accumulator),
+//       {}
+//     );
+
+//     if (typeof obj[key] !== "object") {
+//       newObj[obj[key]] = key;
+//     } else {
+//       newObj[key] = nestedChangedObj;
+//     }
+//   }
+
+//   return newObj;
+// }
+
+// console.log(changeObjectKeysWithValues(obj));
+
 const obj = {
   name: "Jhon",
   country: {
     name: "Armenia",
     code: 374,
+    c: {
+      b: 5000000000,
+    },
   },
 };
 
@@ -20,7 +54,7 @@ function changeObjectKeysWithValues(obj) {
     if (typeof obj[key] !== "object") {
       newObj[obj[key]] = key;
     } else {
-      newObj[key] = nestedChangedObj;
+      newObj[key] = changeObjectKeysWithValues(obj[key]);
     }
   }
 
